d5de74b3806ebc8b37e1f7be60198b66ad8955d8
humayunSean <humayun.sean@gmail.com>
2016-05-26 23:43:10 +0530

    new Project commit

---
+++ app/src/main/java/com/bhi/humayun/udaproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bhi.humayun.udaproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
